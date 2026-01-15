b64d03b3eb6461ad4bdb2ee7a4574e81d2566ae7
Nosezzy <151822668+Alfin452@users.noreply.github.com>
2026-01-15 16:35:17 +0700

    button

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({required this.startQuiz,required this.profile, super.key});
+  const HomeScreen({required this.startQuiz, required this.profile, super.key});
 
   final void Function() startQuiz;
   final void Function() profile;
-
 
   @override
   Widget build(context) {
@@ -22,17 +21,27 @@ class HomeScreen extends StatelessWidget {
           Text(
             'Learn Flutter the Fun Way!',
             style: GoogleFonts.lato(
-              color: Color.fromARGB(255, 215, 196, 249),
+              color: const Color.fromARGB(255, 215, 196, 249),
               fontSize: 22,
               fontWeight: FontWeight.bold,
             ),
           ),
           const SizedBox(height: 30),
+          // Tombol Start Quiz
           OutlinedButton.icon(
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
             onPressed: startQuiz,
             icon: const Icon(Icons.arrow_right_alt),
             label: const Text('Start Quiz'),
+          ),
+          const SizedBox(height: 10), // Jarak antar tombol
+          // Tombol Profile (Baru)
+          OutlinedButton.icon(
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            onPressed:
+                profile, // Memanggil fungsi profile yang dilempar dari Quiz widget
+            icon: const Icon(Icons.person),
+            label: const Text('Profile'),
           ),
         ],
       ),
